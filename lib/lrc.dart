d794a07bdc3de98a33e7c48d6ad73186c2040f90
MSOB7YY <mohamedsobhy201566@gmail.com>
2025-12-03 19:45:39 +0200

    feat: parse ttml lyrics (xml files)

--- lib/lrc.dart
+++ lib/lrc.dart
@@ -1,6 +1,8 @@
 /// The main library for support for LRCs.
 library lrc;
 
+import 'package:html_unescape/html_unescape.dart';
+
 part 'src/core/enums.dart';
 part 'src/core/extensions.dart';
 part 'src/models/lrc.dart';
@@ -9,3 +11,4 @@ part 'src/models/lrc_line_part.dart';
 part 'src/models/lrc_stream.dart';
 part 'src/parsers/lrc_parser.dart';
 part 'src/parsers/multi_timestamp_parser.dart';
+part 'src/parsers/ttml_parser.dart';
---
+++ lib/src/parsers/ttml_parser.dart
@@ -0,0 +1,57 @@
+part of lrc;
+
+class TtmlParser {
+  static final _regexp = RegExp(
+      r'<p[^>]*?begin="([\d:.]+)s?"[^>]*?end="([\d:.]+)s?"[^>]*?>(.*?)<\/p>');
+
+  static bool isValid(String content) => _regexp.hasMatch(content);
+
+  static Lrc parse(String content) {
+    final lrcLines = <LrcLine>[];
+    final personToIndex = <String, int>{};
+    final htmlUnescape = HtmlUnescape();
+    var type = LrcTypes.simple;
+
+    for (final m in _regexp.allMatches(content)) {
+      int? startMS;
+      // int? endMS;
+      try {
+        startMS = (double.parse(m.group(1)!) * 1000).round();
+        // endMS = (double.parse(m.group(2)!) * 1000).round();
+      } catch (_) {}
+      if (startMS != null) {
+        final textOriginal = m.group(3);
+        String textNormalized;
+        if (textOriginal != null && textOriginal.isNotEmpty) {
+          textNormalized = htmlUnescape.convert(textOriginal);
+        } else {
+          textNormalized = '';
+        }
+
+        final personText =
+            textNormalized.substring(0, textNormalized.indexOf('<'));
+        final person = personToIndex[personText] ??= personToIndex.length + 1;
+        var parts =
+            LrcParser.extractTimeStampPartFromLine(textNormalized).toList();
+        final lineType = parts.isNotEmpty ? LrcTypes.enhanced : LrcTypes.simple;
+        if (lineType == LrcTypes.enhanced) type = LrcTypes.enhanced;
+        final lrcLine = LrcLine(
+          timestamp: Duration(milliseconds: startMS),
+          lyrics: textNormalized,
+          readableText: parts.isNotEmpty
+              ? parts.map((e) => e.lyrics).join()
+              : textNormalized,
+          type: lineType,
+          parts: parts,
+          person: person,
+        );
+        lrcLines.add(lrcLine);
+      }
+    }
+
+    return Lrc(
+      lyrics: lrcLines,
+      type: type,
+    );
+  }
+}
